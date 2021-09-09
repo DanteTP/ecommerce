254f159b761191bc3e63dbb11f7f475de1d7ef78
DanteTP <dante_tachella@hotmail.com>
2021-09-09 11:26:21 -0300

    GEneracion vista usuario, finalizacion formato vista register y login. DEfinicion rutas necesarias para el proyecto y creacion 2 modulos para modificacion datos personales y cambio contraseña

--- ecommerce/routes/product.js
+++ ecommerce/routes/product.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+//GET
+// Carga pantalla detalle producto
 router.get('/detail', function(req, res, next) {
   res.render('productdetail', { title: 'Express' });
 })
 
+// Carga pantalla carrita
 router.get('/cart', function(req, res, next) {
   res.render('cart', { title: 'Express' });
 })
+
+// Carga pantalla carga producto
+
+// Carga productos luego de búsqueda
+
+// Carga productos por búsqueda en Menú
+
+//POST
+// Creacion producto nuevo
+
+
+// Pago via Paypal o MercadoPago
+
+// Finalización de la compra y creación del pedido
 
 module.exports = router;
--- ecommerce/routes/users.js
+++ ecommerce/routes/users.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+//GET
+
+// Carga pantalla login
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' })
 });
+
+// Carga pantalla register
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Express' })
 });
 
+// Carga pantalla usuario via cookies
+router.get('/user', function(req, res, next) {
+  res.render('user', { title: 'Express' })
+});
+
+
+// POST
+// Validación y carga pantalla usuario via login
+
+// Validación y carga pantalla usuario via register
+
+// Validación y carga carrito via login
+
+// Registro y carga de carrito via register
 
 module.exports = router;
