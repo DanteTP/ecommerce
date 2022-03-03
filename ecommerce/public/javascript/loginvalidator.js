d192d2f2ac1667d18777409705393ee27012756c
DanteTP <dante_tachella@hotmail.com>
2022-03-03 10:41:27 -0300

    cookie message working properly

--- ecommerce/public/javascript/loginvalidator.js
+++ ecommerce/public/javascript/loginvalidator.js
@@ -1,15 +1,10 @@
+// const { contextsKey } = require("express-validator/src/base");
+
 window.onload = ()=>{
         let loginsubmit = document.getElementById('loginsubmit')
         let cookiecheck = document.getElementById('checkbox')
-        let cookielabel = document.getElementById('checklabel')
 
-        if(cookiecheck=='on'){
-            if(localStorage.getItem('cookie')=='rechazada'){
-                localStorage.setItem('cookie','aceptada')
-            }
-        }
-    
-        
+
         // Login validation
         loginsubmit.addEventListener("click",function (event){
         let msg = document.getElementById('alert')    
@@ -17,8 +12,9 @@ window.onload = ()=>{
         let password = document.getElementById('PasswordL')
         let passpattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
         let emailpattern = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
-
+        
         if( (emailpattern.test(email.value) && passpattern.test(password.value)) && password.value.length>7){
+            cookiecheck.checked?localStorage.setItem('cookie','aceptada'):localStorage.getItem('cookie')=='rechazada'
         }else {
             event.preventDefault()
             msg.innerHTML = `<p style="color: red; font-weight: bold;">Usuario y/o contraseña incorrecto</p>`
